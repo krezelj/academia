7eddd0af57919332ac62fa0a198970871496d96a
Szymon-Gut <s.gucio@wp.pl>
2023-10-09 19:53:11 +0200

    rewritten to pyTorch, polices added

--- src/agents/dqn.py
+++ src/agents/dqn.py
@@ -1,58 +1,106 @@
 import numpy as np
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.optimizers import Adam
+import torch.nn as nn
+import torch
+from .base import Agent
+from typing import Literal
+import torch.optim as optim
+import torch.nn.functional as F
 
+class DQNNetwork(nn.Module):
+    #zastanowic sie nad konwencja
+    def __init__(self, state_size, n_actions, use_convolutions=False):
+        super(DQNNetwork, self).__init__()
+        self.use_convolutions = use_convolutions
+        if use_convolutions:
+            self.network = nn.Sequential(
+                nn.Linear(state_size, 120),
+                nn.ReLU(),
+                nn.Linear(120, 84),
+                nn.ReLU(),
+                nn.Linear(84, n_actions),
+                #zastanowić się jak w przypadku przestrzeni akcji która również jest ciągła
+            )
+        else:
+            self.network = nn.Sequential(
+                nn.Conv2d(4, 32, 8, stride=4),
+                nn.ReLU(),
+                nn.Conv2d(32, 64, 4, stride=2),
+                nn.ReLU(),
+                nn.Conv2d(64, 64, 3, stride=1),
+                nn.ReLU(),
+                nn.Flatten(),
+                nn.Linear(3136, 512),
+                nn.ReLU(),
+                nn.Linear(512, n_actions),
+            )
 
-class DQN:
+class DQN(Agent):
     def __init__(self, state_size: int, 
-                 num_of_actions: int, gamma: float =1., 
+                 policy_type: Literal['CnnPolicy', 'MlpPolicy'],
+                 n_actions: int, gamma: float =1., 
                  epsilon: float =1., epsilon_decay: float =0.99,
                  epsilon_min: float =0.01, learning_rate: float =0.01
                  ):
         self.state_size = state_size
-        self.num_of_actions = num_of_actions
+        self.n_actions = n_actions
         self.memory = np.array([])
         self.gamma = gamma
         self.epsilon = epsilon
         self.epsilon_decay = epsilon_decay
         self.epsilon_min = epsilon_min
         self.learning_rate = learning_rate
-        self.network = self._build_network_()
-        self.target_network = self._build_network_()
+        self.policy_type = policy_type
+        self._build_network_()
 
     def _build_network_(self):
-        network = Sequential()
-        network.add(Dense(24, input_dim=self.state_size, activation='relu'))
-        network.add(Dense(24, activation='relu'))
-        network.add(Dense(self.num_of_actions, activation='linear'))
-        network.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
-        return network
-    
+        if self.policy_type == "CnnPolicy":
+            self.network = DQNNetwork(self.state_size, self.n_actions, use_convolutions=True)
+            self.target_network = DQNNetwork(self.state_size, self.n_actions, use_convolutions=True)
+            self.optimizer = optim.Adam(self.network.parameters(), lr=0.001)
+        else:
+            self.network = DQNNetwork(self.state_size, self.n_actions, use_convolutions=False)
+            self.target_network = DQNNetwork(self.state_size, self.n_actions, use_convolutions=False)
+        self.target_network.load_state_dict(self.network.state_dict())
+        self.target_network.eval()
+
     def remember(self, state, action, reward, next_state, done):
         np.append(self.memory, (state, action, reward, next_state, done))
 
     def get_action(self, state):
         if np.random.uniform() <= self.epsilon:
-            return np.random.randint(0, self.num_of_actions)
+            return np.random.randint(0, self.n_actions)
         else:
-            q_val_act = self.network.predict(state)
-            return np.argmax(q_val_act)
+            q_val_act = self.network(torch.Tensor(state))
+            return torch.argmax(q_val_act).item()
         
     def update_target(self):
-        self.target_network.set_weights(self.network)
+        self.target_network.load_state_dict(self.network.state_dict())
     
+    def update():
+        #zaimplementowac
+        pass
+
     def replay(self, batch_size):
         batch_indices = np.random.choice(len(self.memory), size=batch_size, replace=False)
         batch = [self.memory[i] for i in batch_indices]
+        states, targets = [], []
         for state, action, reward, next_state, done in batch:
             target = reward
             if not done:
-                next_action = np.argmax(self.network.predict(next_state))
-                target = reward + self.gamma * self.target_network.predict(next_state)[next_action]
-            target_function = self.network.predict(state)
-            target_function[0][action] = target
-            self.network.fit(state, target_function, epochs=1, verbose=0)
+                target = reward + self.gamma * torch.max(self.target_network(torch.Tensor(next_state)))
+            target_value = self.network(torch.Tensor(state))
+            target_value[0][action] = target
+            states.append(state)
+            targets.append(target_value)
+        
+        states = torch.Tensor(states)
+        targets = torch.stack(targets)
+        self.optimizer.zero_grad()
+        q_values = self.network(states)
+        loss = F.mse_loss(q_values, targets)
+        loss.backward()
+        self.optimizer.step()
+        
         if self.epsilon > self.epsilon_min:
             self.epsilon = self.epsilon * self.epsilon_decay
         
