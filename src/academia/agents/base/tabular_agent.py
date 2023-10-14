ea9db3ae87f097d5a2244c943bd7670c4d1fcfc1
Szymon-Gut <72514490+Szymon-Gut@users.noreply.github.com>
2023-10-14 15:29:54 +0200

    saving and loading function for tabular agent

--- src/academia/agents/base/tabular_agent.py
+++ src/academia/agents/base/tabular_agent.py
@@ -1,8 +1,7 @@
-import json
-import os
+import yaml
 from abc import ABC
 from collections import defaultdict
-
+import ast
 import numpy as np
 
 from .agent import Agent
@@ -29,29 +28,30 @@ class TabularAgent(Agent, ABC):
         else:
             return np.random.randint(0, self.n_actions)
 
-    def to_json(self, name: str):
+    def save(self, path: str):
         learner_state_dict = {
             'n_actions': self.n_actions,
             'alpha': self.alpha,
             'gamma': self.gamma,
-            'q_table': dict(self.q_table),
+            'q_table': {str(key): str(value.tolist()) for key, value in self.q_table.items()},
             'epsilon': self.epsilon,
             'epsilon_decay': self.epsilon_decay,
             'min_epsilon': self.min_epsilon,
         }
-        save_dir = './saved_agents/'
-        os.makedirs(save_dir, exist_ok=True)
-        with open(f'{save_dir}/TabularAgent-{name}.json', 'w') as file:
-            json.dump(learner_state_dict, file)
+        with open(f'{path}.yaml', 'w') as file:
+            yaml.dump(dict(learner_state_dict), file)
 
-    def load_json(self, name: str):
-        save_dir = './saved_agents/'
-        with open(f'{save_dir}/TabularAgent-{name}.json', 'r') as file:
-            learner_state_dict = json.load(file)
+    def load(self, path: str):
+        with open(f'{path}.yaml', 'r') as file:
+            learner_state_dict = yaml.safe_load(file)
         self.n_actions = learner_state_dict['n_actions']
         self.alpha = learner_state_dict['alpha']
         self.gamma = learner_state_dict['gamma']
-        self.q_table = defaultdict(lambda: np.zeros(self.n_actions), learner_state_dict['q_table'])
+        self.q_table = defaultdict(lambda: np.zeros(self.n_actions))
+        for key, value in learner_state_dict['q_table'].items():
+            self.q_table[eval(key)] = np.array(ast.literal_eval(value))
         self.epsilon = learner_state_dict['epsilon']
         self.epsilon_decay = learner_state_dict['epsilon_decay']
         self.min_epsilon = learner_state_dict['min_epsilon']
+
+    
