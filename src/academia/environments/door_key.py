11d1d3e4ebd2c8c91be9353937bb323353a26cc5
Szymon-Gut <s.gucio@wp.pl>
2023-10-19 22:05:01 +0200

    add minigrid wrapper

--- src/academia/environments/door_key.py
+++ src/academia/environments/door_key.py
@@ -4,9 +4,9 @@ import gymnasium
 import numpy as np
 import numpy.typing as npt
 
-from .base import ScalableEnvironment
+from .base import GenericMiniGridWrapper
 
-class DoorKey(ScalableEnvironment):
+class DoorKey(GenericMiniGridWrapper):
 
     N_ACTIONS = 5
 
@@ -24,41 +24,14 @@ class DoorKey(ScalableEnvironment):
                             and 3 is the hardest
         :param render_mode: render_mode value passed to gymnasium.make
         """
-        super().__init__(difficulty)
-        try:
-            env_id = DoorKey.__difficulty_envid_map[difficulty]
-        except KeyError:
-            msg = (f"Difficulty value of {difficulty} is invalid for this environment. "
-                   "Difficulty level should be an integer between 0 and 3")
-            raise ValueError(msg)
-        self.__base_env = gymnasium.make(env_id, render_mode=render_mode)
-        self.__state_raw = None  # will be set inside self.reset()
-        self.reset()
-        self.STATE_SIZE = len(self.__state)
+        super().__init__(difficulty, DoorKey.__difficulty_envid_map, render_mode=render_mode)
 
-
-    def step(self, action: int) -> tuple[Any, float, bool]:
-        new_state, reward, terminated, truncated, _ = self.__base_env.step(action)
-        self.__state_raw = new_state
-        is_episode_end = terminated or truncated
-        return self.observe(), float(reward), is_episode_end
-
-    def observe(self) -> Any:
-        return self.__state
 
     def get_legal_mask(self) -> npt.NDArray[Union[bool, int]]:
         return np.array([1 for _ in range(self.N_ACTIONS)])
-
-    def reset(self) -> Any:
-        self.__door_status = 2
-        self.__state_raw = self.__base_env.reset()[0]
-        return self.observe()
-    
-    def render(self):
-        return self.__base_env.render()
     
     @property
-    def __state(self) -> tuple[int, ...]:
+    def _state(self) -> tuple[int, ...]:
         cells_obj_types = self.__state_raw['image'][:,:,0]
         cells_flattened = cells_obj_types.flatten()
         direction = self.__state_raw['direction']
