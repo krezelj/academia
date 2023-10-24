f2186864f903b1aa8c4b31ebcf93ea43093ada0f
Szymon-Gut <s.gucio@wp.pl>
2023-10-24 23:54:39 +0200

    added params to __init__ in models

--- src/academia/models/__init__.py
+++ src/academia/models/__init__.py
@@ -1,7 +1,9 @@
 from .cart_pole_mlp import CartPoleMLP
 from .lava_crossing_mlp import LavaCrossingMLP
+from .door_key_mlp import DoorKeyMLP
 
 __all__ = [
     'CartPoleMLP',
     'LavaCrossingMLP',
+    'DoorKeyMLP',
 ]
