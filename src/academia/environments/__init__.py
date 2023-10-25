a8fb24ba37d7adee0e42369942852ce692de4205
Szymon-Gut <s.gucio@wp.pl>
2023-10-25 22:19:52 +0200

    added lunar_lander in __init__.py

--- src/academia/environments/__init__.py
+++ src/academia/environments/__init__.py
@@ -1,9 +1,11 @@
 from .bridge_building import BridgeBuilding
 from .lava_crossing import LavaCrossing
 from .door_key import DoorKey
+from .lunar_lander import LunarLander
 
 __all__ = [
     'BridgeBuilding',
     'LavaCrossing',
     'DoorKey',
+    'LunarLander',
 ]
