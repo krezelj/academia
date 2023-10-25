9d76e7a073cbb642acd0a84269f68a3f9da61d5a
Szymon-Gut <s.gucio@wp.pl>
2023-10-25 22:35:23 +0200

    added MLP arch for lunar lander

---
+++ src/academia/models/lunar_lander_mlp.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+import torch
+
+class LunarLanderMLP(nn.Module):
+
+    def __init__(self):
+        super(LunarLanderMLP, self).__init__()
+        self.network = nn.Sequential(
+            nn.Linear(8, 64),
+            nn.ReLU(),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, 4),
+        )
+
+    def forward(self, x) -> torch.Tensor:
+        return self.network(x)
