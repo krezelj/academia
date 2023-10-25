6c3300217a9d052865fceea975e8012cc1f4278c
Szymon-Gut <s.gucio@wp.pl>
2023-10-25 22:36:00 +0200

    added LunarLander to __init__

--- src/academia/models/__init__.py
+++ src/academia/models/__init__.py
@@ -1,9 +1,11 @@
 from .cart_pole_mlp import CartPoleMLP
 from .lava_crossing_mlp import LavaCrossingMLP
 from .door_key_mlp import DoorKeyMLP
+from .lunar_lander_mlp import LunarLanderMLP
 
 __all__ = [
     'CartPoleMLP',
     'LavaCrossingMLP',
     'DoorKeyMLP',
+    'LunarLanderMLP',
 ]
