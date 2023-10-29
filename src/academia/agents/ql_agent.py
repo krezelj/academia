ef40dccc1df240f655710363677b09218247a0e9
maciejors <maciej.orslowski@gmail.com>
2023-10-29 20:11:04 +0100

    reformat tabular agents update() methods to comply with #36

--- src/academia/agents/ql_agent.py
+++ src/academia/agents/ql_agent.py
@@ -9,5 +9,5 @@ class QLAgent(TabularAgent):
 
     def update(self, state, action, reward, new_state, is_terminal):
         self.q_table[state][action] =\
-            (1-self.alpha)*self.q_table[state][action] +\
-            self.alpha*(reward + self.gamma * np.max(self.q_table[new_state]))
+            (1-self.alpha)*self.q_table[state][action] \
+            + self.alpha*(reward + self.gamma * np.max(self.q_table[new_state]))
