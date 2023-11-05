20f8972ed8df5aefb1aa2098b53f3c4c64361dd1
Szymon-Gut <s.gucio@wp.pl>
2023-11-05 18:37:24 +0100

    update init in utils

--- src/academia/utils/__init__.py
+++ src/academia/utils/__init__.py
@@ -1,7 +1,23 @@
 from .saving_loading import SavableLoadable
 from .stopwatch import Stopwatch
+from .visualizations import (
+    plot_task, 
+    plot_rewards_curriculum, 
+    plot_trajectory_curriculum, 
+    plot_curriculum_vs_nocurriculum, 
+    plot_evaluation_impact, 
+    plot_time_impact, 
+    plot_multiple_evaluation_impact
+)
 
 __all__ = [
     'SavableLoadable',
     'Stopwatch',
+    'plot_task',
+    'plot_rewards_curriculum',
+    'plot_trajectory_curriculum',
+    'plot_curriculum_vs_nocurriculum',
+    'plot_evaluation_impact',
+    'plot_time_impact',
+    'plot_multiple_evaluation_impact',
 ]
