e9f326a1d6b7e2408e7a589782bad462c96b19ab
maciejors <maciej.orslowski@gmail.com>
2023-11-09 17:41:49 +0100

    document agents module

--- academia/agents/__init__.py
+++ academia/agents/__init__.py
@@ -1,3 +1,19 @@
+"""
+This module contains implementations of reinforcement learning algorithms, including tabular and those based
+on neural networks.
+
+Exported classes:
+
+- :class:`QLAgent`
+- :class:`SarsaAgent`
+- :class:`DQNAgent`
+- :class:`PPOAgent`
+
+Note:
+    :class:`DQNAgent` and :class:`PPOAgent` need to be provided network architectures when initialising.
+    These network architectures should be subclasses of ``torch.nn.Module``. Example architectures
+    can be found in :mod:`academia.utils.models`.
+"""
 from .ql_agent import QLAgent
 from .sarsa_agent import SarsaAgent
 from .dqn_agent import DQNAgent
