20ec93f4cb7a89fdabb8211034cda1c2e63927ab
Szymon-Gut <72514490+Szymon-Gut@users.noreply.github.com>
2023-11-12 12:44:03 +0100

    cleaning tests for generic wrapper

--- tests/environments/test_generic_gymnasium_wrapper.py
+++ tests/environments/test_generic_gymnasium_wrapper.py
@@ -2,7 +2,6 @@ import unittest
 from unittest.mock import Mock, patch
 
 import numpy as np
-import gymnasium
 
 from academia.environments import LavaCrossing
 from academia.environments.base import GenericGymnasiumWrapper
@@ -19,46 +18,29 @@ class TestGenericGymnasiumWrapper(unittest.TestCase):
                                            append_step_count=True)
 
     def test_step(self):
-        example_image_observation = np.ones((7, 7, 3), dtype=np.uint8)
-        mocked_step_return = ({
-                              'direction': gymnasium.spaces.Discrete(4).sample(),  # Example discrete direction
-                              'image': example_image_observation,
-                              'mission': 'example_mission'  # Example of mission, not important for testing
-                              }, 0., False, False, {})  # Sample return values, modify based on your actual environment
-        # Mock the base environment's step method
-        with patch.object(self.env._base_env, 'step', return_value=mocked_step_return):
-            initial_state = self.env.observe()
-            action = 0
-            new_state, reward, is_episode_end = self.env.step(action)
+        initial_state = self.env.observe()
+        action = 0
+        new_state, reward, is_episode_end = self.env.step(action)
 
-            self.assertEqual(self.env.step_count, 1)
+        self.assertEqual(self.env.step_count, 1)
 
-            self.assertIsInstance(new_state, np.ndarray)
-            self.assertIsInstance(reward, float)
-            self.assertIsInstance(is_episode_end, bool)
+        self.assertIsInstance(new_state, np.ndarray)
+        self.assertIsInstance(reward, float)
+        self.assertIsInstance(is_episode_end, bool)
 
-            # Ensure the environment state is updated
-            self.assertNotEqual(new_state.tolist(), initial_state.tolist())
+        # Ensure the environment state is updated
+        self.assertNotEqual(new_state.tolist(), initial_state.tolist())
 
     def test_observe(self):
-        # Test the observe method
         state = self.env.observe()
         self.assertIsInstance(state, np.ndarray)
 
     def test_reset(self):
-        example_image_observation = np.ones((7, 7, 3), dtype=np.uint8) * 128
-        mocked_step_return = ({
-                              'direction': gymnasium.spaces.Discrete(4).sample(),  # Example discrete direction
-                              'image': example_image_observation,
-                              'mission': 'example_mission'  # Example of mission, not important for testing
-                              }, 0., False, False, {})  # Sample return values, modify based on your actual environment
-        # Mock the base environment's reset method
-        with patch.object(self.env._base_env, 'reset', return_value=mocked_step_return):
-            initial_state = self.env.reset()
+        initial_state = self.env.reset()
 
-            self.assertEqual(self.env.step_count, 0)
+        self.assertEqual(self.env.step_count, 0)
 
-            self.assertNotEqual(initial_state.tolist(), np.zeros(self.env.STATE_SHAPE).tolist())
+        self.assertNotEqual(initial_state.tolist(), np.zeros(self.env.STATE_SHAPE).tolist())
 
     def test_render(self):
         self.env.render()
@@ -88,57 +70,40 @@ class TestGenericGymnasiumWrapper(unittest.TestCase):
         self.assertEqual(transformed_state.tolist(), [2, 2, 2, 2])
 
     def test_frame_stacking(self):
-        # Test frame stacking
-        example_image_observation = np.ones((7, 7, 3), dtype=np.uint8)
-        mocked_step_return = ({
-                              'direction': gymnasium.spaces.Discrete(4).sample(),  # Example discrete direction
-                              'image': example_image_observation,
-                              'mission': 'example_mission'  # Example of mission, not important for testing
-                              }, 0., False, False, {})  # Sample return values, modify based on your actual environment
-        # Mock the base environment's step method
-        with patch.object(self.env_stacked._base_env, 'step', return_value=mocked_step_return):
-            self.env_stacked.reset()
-            initial_state = self.env_stacked.observe()
-            action = 0
-            new_state, reward, is_episode_end = self.env_stacked.step(action)
+        self.env_stacked.reset()
+        initial_state = self.env_stacked.observe()
+        action = 0
+        new_state, reward, is_episode_end = self.env_stacked.step(action)
 
-            self.assertEqual(self.env_stacked.step_count, 1)
+        self.assertEqual(self.env_stacked.step_count, 1)
 
-            self.assertIsInstance(new_state, np.ndarray)
-            self.assertIsInstance(reward, float)
-            self.assertIsInstance(is_episode_end, bool)
+        self.assertIsInstance(new_state, np.ndarray)
+        self.assertIsInstance(reward, float)
+        self.assertIsInstance(is_episode_end, bool)
 
-            # Ensure the environment state is updated
-            self.assertNotEqual(new_state.tolist(), initial_state.tolist())
+        # Ensure the environment state is updated
+        self.assertNotEqual(new_state.tolist(), initial_state.tolist())
 
-            # Ensure the environment state is stacked
-            self.assertEqual(new_state.shape, ((7 * 7 + 1) * 2,))
+        # Ensure the environment state is stacked
+        self.assertEqual(new_state.shape, ((7 * 7 + 1) * 2,))
 
     def test_step_append(self):
-        example_image_observation = np.ones((7, 7, 3), dtype=np.uint8)
-        mocked_step_return = ({
-                              'direction': gymnasium.spaces.Discrete(4).sample(),  # Example discrete direction
-                              'image': example_image_observation,
-                              'mission': 'example_mission'  # Example of mission, not important for testing
-                              }, 0., False, False, {})
-        # Mock the base environment's step method
-        with patch.object(self.env_step_count._base_env, 'step', return_value=mocked_step_return):
-            self.env_step_count.reset()
-            initial_state = self.env_step_count.observe()
-            action = 0
-            new_state, reward, is_episode_end = self.env_step_count.step(action)
+        self.env_step_count.reset()
+        initial_state = self.env_step_count.observe()
+        action = 0
+        new_state, reward, is_episode_end = self.env_step_count.step(action)
 
-            self.assertEqual(self.env_step_count.step_count, 1)
+        self.assertEqual(self.env_step_count.step_count, 1)
 
-            self.assertIsInstance(new_state, np.ndarray)
-            self.assertIsInstance(reward, float)
-            self.assertIsInstance(is_episode_end, bool)
+        self.assertIsInstance(new_state, np.ndarray)
+        self.assertIsInstance(reward, float)
+        self.assertIsInstance(is_episode_end, bool)
 
-            # Ensure the environment state is updated
-            self.assertNotEqual(new_state.tolist(), initial_state.tolist())
+        # Ensure the environment state is updated
+        self.assertNotEqual(new_state.tolist(), initial_state.tolist())
 
-            # Ensure the environment state is stacked
-            self.assertEqual(new_state.shape, ((7 * 7 + 1) * 1 + 1,))
+        # Ensure the environment state is stacked
+        self.assertEqual(new_state.shape, ((7 * 7 + 1) * 1 + 1,))
 
 
 if __name__ == '__main__':
