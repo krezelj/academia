be4729d79db57519af8241cc5f731f675fe1e913
Szymon-Gut <72514490+Szymon-Gut@users.noreply.github.com>
2023-11-12 14:44:36 +0100

    typo in comments in test_door_key.py

--- tests/environments/test_door_key.py
+++ tests/environments/test_door_key.py
@@ -15,12 +15,12 @@ class TestDoorKey(unittest.TestCase):
         # Test the _transform_action method
         state = self.env.reset()
         # If mapping is correct, action 4 should be mapped to action 5 in the underlying environment
-        # and agent should change his state, otherwise performing unallowed action state should be the same
+        # and agent should change his state, otherwise performing not allowed action state should be the same
         new_state, reward, is_episode_end = self.env.step(1)
         self.assertNotEqual(state.tolist(), new_state.tolist())
 
         state = self.env.reset()
-        new_state, reward, is_episode_end = self.env.step(6)  # Unallowed action
+        new_state, reward, is_episode_end = self.env.step(6)  # Not allowed action
         self.assertEqual(state.tolist(), new_state.tolist())
 
     def test_observe(self):
@@ -40,7 +40,7 @@ class TestDoorKey(unittest.TestCase):
                                   'mission': 'example_mission'  # Example of mission, not important for testing
                               }, 0., False, False, {})
         with patch.object(self.env._base_env, 'step', return_value=mocked_step_return):
-            new_state, reward, is_episode_end = self.env.step(0)
+            self.env.step(0)
             self.assertEqual(self.env._door_status, 1)  # Door opened
 
         example_image_observation = np.zeros((7, 7, 3), dtype=np.uint8)
@@ -50,7 +50,7 @@ class TestDoorKey(unittest.TestCase):
                                   'mission': 'example_mission'  # Example of mission, not important for testing
                               }, 0., False, False, {})
         with patch.object(self.env._base_env, 'step', return_value=mocked_step_return):
-            new_state, reward, is_episode_end = self.env.step(0)
+            self.env.step(0)
             self.assertEqual(self.env._door_status, 0)
 
     def test_invalid_difficulty(self):
