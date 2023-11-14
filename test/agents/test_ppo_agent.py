dceab0108fd0e119b66ce525d249060001a34284
Jasiek <jasiekk9@gmail.com>
2023-11-14 11:04:45 +0100

    Add tests for `PPOAgent` class

--- test/agents/test_ppo_agent.py
+++ test/agents/test_ppo_agent.py
@@ -40,21 +40,38 @@ class TestPPOAgent(unittest.TestCase):
                 return False
         return True
 
-    def __is_buffer_equal(self, expected, returned):
+    def __is_buffer_equal(self, expected: PPOAgent.PPOBuffer, returned: PPOAgent.PPOBuffer):
         """
-        Checks is two buffers are the same ignoring unfinished episodes.
+        Checks if two buffers are the same ignoring unfinished episodes.
         """
-        # for expected_transition, loaded_transition in zip(expected, loaded):
-        #     for expected_element, loaded_element in zip(expected_transition, loaded_transition):
-        #         if type(expected_element) is np.ndarray:
-        #             if not np.all(expected_element == loaded_element):
-        #                 return False
-        #         else:
-        #             if expected_element != loaded_element:
-        #                 return False
+
+        # we can only check up to the last finished episode
+        n_valid_steps = int(np.sum(expected.episode_lengths).item())
+
+        self.assertEqual(expected.episode_counter, returned.episode_counter)
+        self.assertEqual(len(expected.episode_lengths), len(returned.episode_lengths))
+        self.assertEqual(n_valid_steps, returned.steps_counter)
+        self.assertEqual(expected.n_steps, returned.n_steps)
+        self.assertEqual(expected.n_episodes, returned.n_episodes)
+        
+        for expected_transitions, returned_transitions \
+                in zip(expected.get_tensors('cpu'), returned.get_tensors('cpu')):
+            for i, (expected_element, returned_element) \
+                    in enumerate(zip(expected_transitions, returned_transitions)):
+                if i >= n_valid_steps:
+                    break
+                if type(expected_element) is np.ndarray:
+                    if not np.all(expected_element == returned_element):
+                        return False
+                if type(expected_element) is torch.Tensor:
+                    if not torch.all(expected_element == returned_element):
+                        return False
+                else:
+                    if expected_element != returned_element:
+                        return False
         return True
 
-    def __assert_agents_equal(self, expected, returned):
+    def __assert_agents_equal(self, expected: PPOAgent, returned: PPOAgent):
         self.assertTrue(self.__is_model_equal(expected.actor, returned.actor))
         self.assertTrue(self.__is_model_equal(expected.critic, returned.critic))
         self.assertTrue(self.__is_buffer_equal(expected.buffer, returned.buffer))
@@ -244,9 +261,181 @@ class TestPPOAgent(unittest.TestCase):
         # assert
         self.__assert_agents_equal(agent, loaded_agent)
 
-
 class TestPPOBuffer(unittest.TestCase):
-    pass
+    
+    def __fill_buffer(self, buffer: PPOAgent.PPOBuffer, n_samples: int, terminal: bool=False) -> bool:
+        """
+        Fills the buffer with mock data and returns the value returned by the last update
+        """
+        return_flag = None
+        for _ in range(n_samples):
+            return_flag = buffer.update(
+                np.random.randint(0, 10, size=128),
+                np.random.randint(0, 3),
+                np.random.random(),
+                np.random.random(),
+                is_terminal = terminal
+            )
+        return return_flag
+
+    def test_episode_counter(self):
+        # arrange
+        buffer = PPOAgent.PPOBuffer(n_steps=1000)
+        # act/assert
+        self.assertEqual(buffer.episode_counter, 0)
+        self.__fill_buffer(buffer, 5, terminal=False)
+        self.assertEqual(buffer.episode_counter, 0)
+        self.__fill_buffer(buffer, 1, terminal=True)
+        self.assertEqual(buffer.episode_counter, 1)
+        self.__fill_buffer(buffer, 1, terminal=True)
+        self.assertEqual(buffer.episode_counter, 2)
+        self.__fill_buffer(buffer, 1, terminal=False)
+        self.assertEqual(buffer.episode_counter, 2)
+
+    def test_episode_length_counter(self):
+        # arrange
+        buffer = PPOAgent.PPOBuffer(n_steps=1000)
+        # act/assert
+        self.assertEqual(buffer.episode_length_counter, 0)
+        self.__fill_buffer(buffer, 5, terminal=False)
+        self.assertEqual(buffer.episode_length_counter, 5)
+        self.__fill_buffer(buffer, 1, terminal=True)
+        self.assertEqual(buffer.episode_length_counter, 0)
+        self.__fill_buffer(buffer, 1, terminal=False)
+        self.assertEqual(buffer.episode_length_counter, 1)
+
+    def test_episode_lengths(self):
+        # arrange
+        buffer = PPOAgent.PPOBuffer(n_steps=1000)
+        # act/assert
+        self.assertEqual(len(buffer.episode_lengths), 0)
+        self.__fill_buffer(buffer, 5, terminal=False)
+        self.assertEqual(len(buffer.episode_lengths), 0)
+        self.__fill_buffer(buffer, 1, terminal=True)
+        self.assertEqual(len(buffer.episode_lengths), 1)
+        self.assertEqual(buffer.episode_lengths[-1], 6)
+
+        self.__fill_buffer(buffer, 1, terminal=True)
+        self.assertEqual(len(buffer.episode_lengths), 2)
+        self.assertEqual(buffer.episode_lengths[-1], 1)
+
+        self.__fill_buffer(buffer, 5, terminal=False)
+        self.assertEqual(len(buffer.episode_lengths), 2)
+
+    def test_update(self):
+        # arrange
+        buffer = PPOAgent.PPOBuffer(n_steps=1000)
+
+        # act/assert
+        self.assertEqual(len(buffer.states), 0)
+        self.assertEqual(len(buffer.actions), 0)
+        self.assertEqual(len(buffer.actions_logits), 0)
+        self.assertEqual(len(buffer.rewards), 0)
+        self.assertEqual(buffer.steps_counter, 0)
+
+        self.__fill_buffer(buffer, 10)
+
+        self.assertEqual(len(buffer.states), 10)
+        self.assertEqual(len(buffer.actions), 10)
+        self.assertEqual(len(buffer.actions_logits), 10)
+        self.assertEqual(len(buffer.rewards), 10)
+        self.assertEqual(buffer.steps_counter, 10)
+
+    def test_update_return_when_using_n_episodes(self):
+        # arrange
+        buffer = PPOAgent.PPOBuffer(n_episodes=3)
+
+        # act/assert
+        self.assertFalse(self.__fill_buffer(buffer, 1, terminal=True))
+        self.assertFalse(self.__fill_buffer(buffer, 1, terminal=True))
+        self.assertTrue(self.__fill_buffer(buffer, 1, terminal=True))
+    
+    def test_update_return_when_using_n_steps(self):
+        # arrange
+        buffer = PPOAgent.PPOBuffer(n_steps=10)
+
+        # act/assert
+        self.assertFalse(self.__fill_buffer(buffer, 9, terminal=False))
+        self.assertTrue(self.__fill_buffer(buffer, 1, terminal=True))
+
+        self.assertFalse(self.__fill_buffer(buffer, 10, terminal=False))
+        self.assertFalse(self.__fill_buffer(buffer, 1, terminal=False))
+        self.assertTrue(self.__fill_buffer(buffer, 1, terminal=True))
+
+    def test_calculate_rewards_to_go(self):
+        # arrange
+        buffer = PPOAgent.PPOBuffer(n_steps=10)
+        
+        # act
+        self.__fill_buffer(buffer, 2, terminal=False)
+        self.__fill_buffer(buffer, 1, terminal=True)
+        self.__fill_buffer(buffer, 2, terminal=False)
+        self.__fill_buffer(buffer, 1, terminal=True)
+        buffer.calculate_rewards_to_go(gamma=0.8)
+
+        # assert
+        self.assertEqual(len(buffer.rewards_to_go), 6)
+
+        # only test the oldest ones as they depend on other
+        # rewards-to-go-as well so it implictly tests
+        # them as well
+        self.assertAlmostEqual(buffer.rewards_to_go[0],
+                         buffer.rewards[2] * 0.8**2\
+                        +buffer.rewards[1] * 0.8\
+                        +buffer.rewards[0], 5)
+        self.assertAlmostEqual(buffer.rewards_to_go[3],
+                         buffer.rewards[-1] * 0.8**2\
+                        +buffer.rewards[-2] * 0.8\
+                        +buffer.rewards[-3], 5)
+        
+    def test_reset(self):
+        # arrange
+        buffer = PPOAgent.PPOBuffer(n_steps=10)
+
+        # act/assert
+        self.assertEqual(len(buffer.states), 0)
+        self.assertEqual(len(buffer.actions), 0)
+        self.assertEqual(len(buffer.actions_logits), 0)
+        self.assertEqual(len(buffer.rewards), 0)
+        self.assertEqual(len(buffer.episode_lengths), 0)
+        self.assertEqual(buffer.steps_counter, 0)
+        self.assertEqual(buffer.episode_counter, 0)
+        self.assertEqual(buffer.episode_length_counter, 0)
+
+        self.__fill_buffer(buffer, 10)
+        buffer.reset()
+
+        self.assertEqual(len(buffer.states), 0)
+        self.assertEqual(len(buffer.actions), 0)
+        self.assertEqual(len(buffer.actions_logits), 0)
+        self.assertEqual(len(buffer.rewards), 0)
+        self.assertEqual(len(buffer.episode_lengths), 0)
+        self.assertEqual(buffer.steps_counter, 0)
+        self.assertEqual(buffer.episode_counter, 0)
+        self.assertEqual(buffer.episode_length_counter, 0)
+
+    def test_get_tensors(self):
+        # arrange
+        buffer = PPOAgent.PPOBuffer(n_steps=1000)
+        self.__fill_buffer(buffer, 9)
+        self.__fill_buffer(buffer, 1, terminal=True)
+
+        # act
+        buffer.calculate_rewards_to_go(gamma=0.8)
+        states, actions, logits, rtgs = buffer.get_tensors('cpu')
+
+        # assert
+        self.assertEqual(type(states), torch.Tensor)
+        self.assertEqual(states.shape, (10, 128))
+
+        self.assertEqual(type(actions), torch.Tensor)
+        self.assertEqual(actions.shape, (10,))
+
+        self.assertEqual(type(logits), torch.Tensor)
+        self.assertEqual(logits.shape, (10,))
+
+        self.assertEqual(type(rtgs), torch.Tensor)
+        self.assertEqual(rtgs.shape, (10,))
 
 if __name__ == '__main__':
     unittest.main()
