09a5a4449b85f446a2aa0334c27e50c3be9f811b
Jasiek <jasiekk9@gmail.com>
2023-11-17 07:54:45 +0100

    Add tests for `AgentDebugger` class

--- test/tools/test_agent_debugger.py
+++ test/tools/test_agent_debugger.py
@@ -1,71 +1,198 @@
-import threading
-
+from typing import Any, Callable
 import unittest
 from unittest import mock
+from collections import deque
+
+import numpy as np
+
 from academia.tools import AgentDebugger
+from academia.environments import BridgeBuilding
+from academia.agents import QLAgent
 
-class StoppableThread(threading.Thread):
-    """Thread class with a stop() method. The thread itself has to check
-    regularly for the stopped() condition.
-    Source: https://stackoverflow.com/questions/323972/is-there-any-way-to-kill-a-thread
+
+class MockEnvironment:
+    """
+    Mock environment that always terminates if action ``BridgeBuilding.N_ACTIONS`` 
+    was taken (to avoid agent accidentally taking this action) and never terminates otherwise.
     """
 
-    def __init__(self,  *args, **kwargs):
-        super(StoppableThread, self).__init__(*args, **kwargs)
-        self._stop_event = threading.Event()
+    def __init__(self):
+        self.reset()
+    
+    def reset(self):
+        self.state = ""
 
-    def stop(self):
-        self._stop_event.set()
+    def step(self, action):
+        return self.state, 0, action == BridgeBuilding.N_ACTIONS
+    
+    def get_legal_mask(self):
+        return np.ones(shape=BridgeBuilding.N_ACTIONS)
 
-    def stopped(self):
-        return self._stop_event.is_set()
 
-class async_debugger:
-    """
-    Asynchronous agent debugger
-    """
+class InjectedAsserter:
 
-    def __init__(self, debugger: AgentDebugger):
-        self.thread = StoppableThread(target=debugger.run, args=(5,))
+    def __init__(self, 
+                 tester: unittest.TestCase, 
+                 debugger: AgentDebugger,
+                 assertions: list[Callable],
+                 input_sequence: list[str]):
+        self.tester = tester
+        self.debugger = debugger
+        self.assertions = deque(assertions)
+        self.input_sequence = deque(input_sequence)
 
-    def __enter__(self):
-        self.thread.start()
+    def __call__(self, *args, **kwargs):
+        self.assertions.popleft()(self.tester, self.debugger)
+        if len(self.input_sequence) > 0:
+            return self.input_sequence.popleft(), False
+        return '\x1b', True
 
-    def __exit__(self, *exc):
-        self.thread.stop()
-        self.thread.join()
 
 class TestAgentDebugger(unittest.TestCase):
-    
-    def test_paused(self):
+
+    @staticmethod
+    def assert_nothing(teset, agent):
+        """
+        dummy asserter that always passes
+        """
         pass
+
+    @staticmethod
+    def assert_attribute_equal_factory(attribute_name: str, expected: Any):
+        def dynamic_assert(tester: unittest.TestCase, debugger: AgentDebugger):
+            tester.assertEqual(expected, getattr(debugger, attribute_name),
+                               msg=f"wrong value of {attribute_name}")
+        return dynamic_assert
+
+    def setUp(self) -> None:
+        self.agent = QLAgent(n_actions=BridgeBuilding.N_ACTIONS)
+        self.env = BridgeBuilding()
+    
+    def test_pausing(self):
+        # arrange
+        sut = AgentDebugger(self.agent, self.env)
+        injected_asserter = InjectedAsserter(self, sut, [
+            self.assert_attribute_equal_factory("paused", False),
+            self.assert_attribute_equal_factory("paused", True),
+            self.assert_attribute_equal_factory("paused", False),
+        ], input_sequence=['p', 'p', '\x1b'])
+
+        # assert
+        self.assertFalse(sut.paused)
+        with mock.patch('academia.tools.agent_debugger.timedKey', new=injected_asserter):
+            sut.run()
 
     def test_start_paused(self):
-        pass
+        # arrange
+        sut = AgentDebugger(self.agent, self.env, start_paused=True)
+        injected_asserter = InjectedAsserter(self, sut, [
+            self.assert_attribute_equal_factory("paused", True),
+        ], input_sequence=['\x1b'])
 
-    def test_greedy(self):
-        pass
+        # assert
+        with mock.patch('academia.tools.agent_debugger.timedKey', new=injected_asserter):
+            sut.run()
+
+    def test_toggle_greedy(self):
+        # arrange
+        sut = AgentDebugger(self.agent, self.env)
+        injected_asserter = InjectedAsserter(self, sut, [
+            self.assert_attribute_equal_factory("greedy", False),
+            self.assert_attribute_equal_factory("greedy", True),
+            self.assert_attribute_equal_factory("greedy", False),
+        ], input_sequence=['g', 'g', '\x1b'])
+
+        # assert
+        self.assertFalse(sut.paused)
+        with mock.patch('academia.tools.agent_debugger.timedKey', new=injected_asserter):
+            sut.run()
 
     def test_start_greedy(self):
-        pass
+        # arrange
+        sut = AgentDebugger(self.agent, self.env, start_greedy=True)
+        injected_asserter = InjectedAsserter(self, sut, [
+            self.assert_attribute_equal_factory("greedy", True),
+        ], input_sequence=['\x1b'])
 
-    def test_key_action_map(self):
-        pass
-
-    def test_invalid_key_action_map(self):
-        pass
+        # assert
+        with mock.patch('academia.tools.agent_debugger.timedKey', new=injected_asserter):
+            sut.run()
 
     def test_step(self):
-        pass
+        sut = AgentDebugger(self.agent, self.env, start_paused=True)
+        injected_asserter = InjectedAsserter(self, sut, [
+            self.assert_attribute_equal_factory("steps", 1),
+            self.assert_attribute_equal_factory("steps", 2),
+        ], input_sequence=[' ', '\x1b'])
+
+        # assert
+        # prevent episode from terminating
+        with mock.patch.object(BridgeBuilding, 'reset', return_value=("", 0.0, False)):
+            with mock.patch('academia.tools.agent_debugger.timedKey', new=injected_asserter):
+                sut.run()
 
     def test_terminate(self):
-        pass
+        sut = AgentDebugger(self.agent, self.env, start_paused=True)
+        injected_asserter = InjectedAsserter(self, sut, [
+            self.assert_attribute_equal_factory("episodes", 1),
+            self.assert_attribute_equal_factory("episodes", 2),
+        ], input_sequence=['t', '\x1b'])
+
+        # assert
+        with mock.patch('academia.tools.agent_debugger.timedKey', new=injected_asserter):
+            sut.run()
 
     def test_quit(self):
-        pass
+        sut = AgentDebugger(self.agent, self.env, start_paused=True)
+        injected_asserter = InjectedAsserter(self, sut, [
+            self.assert_attribute_equal_factory("running", True),
+        ], input_sequence=['\x1b'])
 
-    def test_thought_handler(self):
-        pass
+        # assert
+        self.assertFalse(sut.running)
+        with mock.patch('academia.tools.agent_debugger.timedKey', new=injected_asserter):
+            sut.run()
+        self.assertFalse(sut.running)
+
+    def test_autorun(self):
+        injected_asserter = InjectedAsserter(self, None, [
+            self.assert_nothing
+        ], input_sequence=['\x1b'])
+
+        # assert
+        with mock.patch('academia.tools.agent_debugger.timedKey', new=injected_asserter):
+            sut = AgentDebugger(self.agent, self.env, start_paused=True, run=True)
+        self.assertGreaterEqual(sut.steps, 0)
+
+    def test_key_action_map(self):
+        env = MockEnvironment()
+        sut = AgentDebugger(self.agent, env, start_paused=True, 
+            key_action_map={'a': BridgeBuilding.N_ACTIONS, 'b': 1}
+        )
+        injected_asserter = InjectedAsserter(self, sut, [
+            self.assert_attribute_equal_factory("episodes", 1),  #a terminate
+            self.assert_attribute_equal_factory("episodes", 2),  #b step     
+            self.assert_attribute_equal_factory("episodes", 2),  #b step
+            self.assert_attribute_equal_factory("steps", 3),     #c (maps to None) step
+            self.assert_attribute_equal_factory("steps", 4),     #a terminate
+            self.assert_attribute_equal_factory("episodes", 3),  #0 terminate
+            self.assert_attribute_equal_factory("episodes", 4),  #1 step
+            self.assert_attribute_equal_factory("episodes", 4),  #c (maps to None) step
+            self.assert_attribute_equal_factory("episodes", 4),  #quit
+        ], input_sequence=['a', 'b', 'b', 'c', 'a', f'{BridgeBuilding.N_ACTIONS}', '1', 'c', '\x1b'])
+
+        # assert
+        with mock.patch('academia.tools.agent_debugger.timedKey', new=injected_asserter):
+            sut.run()
+
+    def test_invalid_key_action_map(self):
+        # check that each key fails separately
+        for reserved_key in AgentDebugger.reserved_keys:
+            with self.assertRaises(ValueError):
+                AgentDebugger(self.agent, self.env, start_paused=True, key_action_map={
+                    reserved_key: 0
+                })
 
 if __name__ == '__main__':
     unittest.main()
+    
